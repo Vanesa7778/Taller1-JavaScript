4d7bb5ff7c200bbbb6b3b57eb64d72ecde63ac0b
Vanesa7778 <129073948+Vanesa7778@users.noreply.github.com>
2023-10-14 17:19:34 -0500

    cambio

---
+++ RestauranteVirtual.js/js/restaurante.js
@@ -0,0 +1,25 @@
+function pedido() {
+    var tipoPlato = document.getElementById("tipoPlato").value;
+    var cantidadPlatos = parseInt(document.getElementById("cantidadPlatos").value);
+    var tipoBebida = document.getElementById("bebida").value;
+    var cantidadBebidas = parseInt(document.getElementById("cantidadBebidas").value);
+
+    var precioPlatos = {
+        "Hamburguesa": 20000,
+        "Salchipapa": 30000,
+        "Perro Caliente": 12000
+    };
+
+    var precioBebida = {
+        "Coca Cola":4500,
+        "Jugos Hit":3500,
+        "Limonada":2000
+    };
+
+    var totalPlatos = precioPlatos[tipoPlato] * cantidadPlatos;
+    var totalBebidas = precioBebida * cantidadBebidas;
+    var total = totalPlatos + totalBebidas;
+
+    var resultado = document.getElementById("resultado");
+    resultado.textContent = "Total a pagar: $" + total;
+}
